dd0a1002932f213531637c79463424db603fe889
Paul Anthony McGowan <xhostcom@aol.com>
2023-01-17 10:57:44 +0000

    Hand class

--- poker.rb
+++ poker.rb
@@ -3,27 +3,41 @@ class Card
     def initialize(face, suit) 
         @face = face
         @suit = suit
-    
     end
 
-end
+end 
+
+class Hand 
+    def initialize(cards)
+        @cards = cards
+    end 
+
+    def show_hand 
+    @cards.each do |card|
+        puts "#{card.face} of #{card.suit}"
+    end
+    end
+end 
+
 
 
 class Poker 
     def initialize 
+        @game_over = false
         @deck = []
-        @suits = ['clubs', 'diamonds', 'hearts', 'spades']
-        @faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-        @suits.each do |suit|
-            @faces.each do |face|
-                @deck << Card.new(face, suit)
-            end
+        ['2','3','4','5','6','7','8','9','10','J','Q','K','A'].each do |face|
+        ['clubs', 'diamonds', 'hearts', 'spades'].each do |suit|
+        @deck.push(Card.new(face, suit))
+            end 
         end
-        @deck.shuffle!
-        puts @deck.inspect
+        @deck = @deck.shuffle!
+        @player_hand = Hand.new(@deck.take(5))
+        @dealer_hand = Hand.new(@deck.take(5))
+        @player_hand.show_hand
 
+        while !@game_over
+        end
     end
-
 end 
 
 Poker.new
